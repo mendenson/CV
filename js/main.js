88441b235616bc7b875093c3756aa94f403fd899
Mendenson Mota <mendenson@gmail.com>
2022-08-27 20:32:44 -0600

    Update CV

--- js/main.js
+++ js/main.js
@@ -28,12 +28,49 @@ function linkAction(){
 
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+
+navLink.forEach((n) => {
+    n.addEventListener('click', linkAction)
+})
 
 /*========================== ACCORDION SKILLS =================================*/
+const skillsContent = document.querySelector('#skills')
+const skillsHeader = document.querySelector('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = "skills__content skills__close"
+    }
+    if(itemClass === "skills__content skills__close"){
+        this.parentNode.className = "skills__content skills__open"
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 
 /*========================== QUALIFICATION TABS =================================*/
 
 
 /*========================== SERVICES MODAL =================================*/
+
+
+/*========================== PORTFOLIO SWIPER =================================*/
+
+
+/*========================== TESTIMONIAL =================================*/
+
+
+/*========================== SCROLL SECTIONS ACTIVE LINK =================================*/
+
+
+/*========================== CHANGE BACKGROUND HEADER =================================*/
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
